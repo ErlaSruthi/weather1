12cb22090a95cd29a830113ff07b97dd41f71c62
SruthiErla <n180051@rguktn.ac.in>
2024-10-28 14:21:56 +0530

    weather api project pushing to github

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Weather_comp from './components/Weather_comp'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather_comp/>
+    </div>
+  )
+}
+export default App
